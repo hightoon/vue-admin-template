bea91b8e4c733dd991edb01cbed8883889a97423
hachen <haitong.chen@booking.com>
2022-08-25 07:27:25 +0800

    add device raw data table header

--- src/views/table/rawdevicedata.vue
+++ src/views/table/rawdevicedata.vue
@@ -8,54 +8,9 @@
       fit
       highlight-current-row
     >
-      <el-table-column align="center" label="序号" width="100">
+      <el-table-column v-for="attribute in tableThread" :key="attribute" :label="attribute" width="100">
         <template slot-scope="scope">
-          {{ scope.$index + 1 }}
-        </template>
-      </el-table-column>
-      <el-table-column align="center" label="设备序列号" width="150">
-        <template slot-scope="scope">
-          {{ scope.row[0] }}
-        </template>
-      </el-table-column>
-      <el-table-column label="时间" width="150">
-        <template slot-scope="scope">
-          {{ scope.row[1] }}
-        </template>
-      </el-table-column>
-      <el-table-column label="设备序号" width="150" align="center">
-        <template slot-scope="scope">
-          <span>{{ scope.row[2] }}</span>
-        </template>
-      </el-table-column>
-      <el-table-column label="硬件版本号" width="150" align="center">
-        <template slot-scope="scope">
-          {{ scope.row[3] }}
-        </template>
-      </el-table-column>
-      <el-table-column label="总运行次数" width="150" align="center">
-        <template slot-scope="scope">
-          {{ scope.row[4] }}
-        </template>
-      </el-table-column>
-      <el-table-column label="软件版本号" width="150" align="center">
-        <template slot-scope="scope">
-          {{ scope.row[4] }}
-        </template>
-      </el-table-column>
-      <el-table-column label="总运行时间(小时)" width="150" align="center">
-        <template slot-scope="scope">
-          {{ scope.row[4] }}
-        </template>
-      </el-table-column>
-      <el-table-column label="总上电时间(小时)" width="150" align="center">
-        <template slot-scope="scope">
-          {{ scope.row[4] }}
-        </template>
-      </el-table-column>
-      <el-table-column label="总计故障次数" width="150" align="center">
-        <template slot-scope="scope">
-          {{ scope.row[4] }}
+          {{ scope.row[attribute] }}
         </template>
       </el-table-column>
     </el-table>
@@ -65,6 +20,16 @@
 <script>
 import { getDevices, getTimeSeries, getDeviceInfo } from '@/utils/tbclient'
 import { getTBToken } from '@/utils/auth'
+
+const fullTableThread = ['设备序号', '设备序列号', '硬件版本号', '软件版本号', '总运行时间', '总上电时间',
+  '总运行次数', '总故障次数', '操作模式', '运行模式', '喘振模式', '系统状态', '故障代码', '警告代码', '当前设定值',
+  '排放压力', '过滤阻力', '入口流量', '入口温度', '出口温度', '电机温度', '变频器温度', '电机转速', '变频器母线电压',
+  '电机电流', '电机功率', '电网电流', '电网功率', '喘振保护纸', '累计电量']
+
+const fakeData = [
+  {设备序号: 1, 设备序列号: 'abc-123', 硬件版本号: '1.0', 软件版本号: '2.0', 总运行时间: 10, 总上电时间: 100,
+  总运行次数: 200, 总故障次数: 100},
+]
 
 export default {
   filters: {
@@ -80,7 +45,8 @@ export default {
   data() {
     return {
       list: null,
-      listLoading: true
+      listLoading: true,
+      tableThread: fullTableThread
     }
   },
   created() {
@@ -89,7 +55,7 @@ export default {
   methods: {
     fetchData() {
       this.listLoading = true
-      this.list = []
+      this.list = fakeData
       this.listLoading = false
       /* getList().then(response => {
         this.list = response.data.items
